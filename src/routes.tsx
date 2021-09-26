a797b04eeffcc0504621d93dd86921e633f9bb67
rogendouglas <rogendouglas@hotmail.com>
2021-09-26 15:47:30 -0500

    feat: created file routes.

---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+
+import { isAuthenticated } from "./services/auth";
+
+type PrivateRouteProps = React.PropsWithChildren<any>;
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  component: Component,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Login</h1>} />
+      <Route path="/signup" component={SignUp} />
+      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+      <Route path="*" component={() => <h1>Pagina não encontrada.</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
